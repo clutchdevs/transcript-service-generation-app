1b0be7ccae8ab69c0480daf730eb0e6fe5503440
baronrafael <baronrafael@hotmail.com>
2025-07-12 00:52:26 -0300

    Adds authentication feature module
    Sets up the basic authentication feature with routing.
    This includes adding the forgot password and register pages.
    The application now redirects to the auth module by default.

---
+++ src/app/features/auth/routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+
+export const authRoutes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/login/login').then((m) => m.Login),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./pages/register/register').then((m) => m.Register),
+  },
+  {
+    path: 'forgot-password',
+    loadComponent: () =>
+      import('./pages/forgot-password/forgot-password').then(
+        (m) => m.ForgotPassword
+      ),
+  },
+];
