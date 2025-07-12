63a2da90a493de2191a616221c5cbdf83cc08ee3
baronrafael <baronrafael@hotmail.com>
2025-07-12 00:01:31 -0300

    Migrates to Jest for unit testing
    Replaces Karma with Jest as the unit testing framework.
    This change offers improved performance and a more modern testing experience.
    It also sets up basic configurations and an example test case.

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,23 +1,11 @@
-import { TestBed } from '@angular/core/testing';
+import { render, screen } from '@testing-library/angular';
 import { App } from './app';
 
-describe('App', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [App],
-    }).compileComponents();
-  });
+describe('AppComponent', () => {
+  it('should render the title', async () => {
+    await render(App);
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, transcript-service-generation-app');
+    const title = screen.getByRole('heading', {name:'Hello, transcript-service-generation-app'})
+    expect(title).toBeTruthy();
   });
 });
