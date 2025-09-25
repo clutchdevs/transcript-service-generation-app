896b9a487cb4ec9318b25664254f61931f2fd6d6
baronrafael <baronrafael@hotmail.com>
2025-09-25 11:52:51 -0400

    feat(dashboard): create new transcription page component logic

--- src/app/features/dashboard/pages/new-transcription/new-transcription.ts
+++ src/app/features/dashboard/pages/new-transcription/new-transcription.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Button } from '../../../../shared/components/ui/button/button';
+import { LANGUAGES, OPERATING_POINTS } from '../../../../core/integrations/speechmatics/constants';
 
 @Component({
   selector: 'app-new-transcription',
-  imports: [],
+  imports: [CommonModule, Button],
   templateUrl: './new-transcription.html',
   styleUrl: './new-transcription.scss'
 })
 export class NewTranscription {
-
+  readonly languages = LANGUAGES;
+  readonly operatingPoints = OPERATING_POINTS;
 }
